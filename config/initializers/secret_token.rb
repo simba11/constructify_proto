0fc1ef366a6b3d6b34df9718aca46fa369829305
Sim Brar <sim@simbrar.com>
2014-07-09 12:08:32 -0700

    added and configured bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Constructify::Application.config.secret_key_base = 'c81e8a6b43713ca6df0aacb2039f876c697a41bfa2a449db05fdf0706ba4434d341c83c2ec02ea7d46f0a424e112cb1e50b233e826928b4838ab3b5a74dec4ef'
